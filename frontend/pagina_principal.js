23efb7de3432fb5f36025d4116be1bb1ec694f26
saul14-alt <lorenzomarinezsaul@gmail.com>
2025-04-11 13:44:25 -0400

    cree el modal de login ya saben

---
+++ frontend/pagina_principal.js
@@ -0,0 +1,18 @@
+
+//FUNCION DEL MODAL
+
+function abrirModal() {
+    document.getElementById("modalLogin").style.display = "flex";
+  }
+
+  function cerrarModal() {
+    document.getElementById("modalLogin").style.display = "none";
+  }
+
+  // Cierra al hacer clic fuera del modal
+  window.onclick = function(e) {
+    const modal = document.getElementById("modalLogin");
+    if (e.target === modal) {
+      cerrarModal();
+    }
+  }
