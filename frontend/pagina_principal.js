60214b1c1a0606929ab851f649c912f31fbefd21
saul14-alt <lorenzomarinezsaul@gmail.com>
2025-04-16 09:04:03 -0400

    puse un poco der responsive en la pagina principal

--- frontend/pagina_principal.js
+++ frontend/pagina_principal.js
@@ -17,3 +17,32 @@ function abrirModal() {
       cerrarModal();
     }
   }
+
+
+function abrirModal2() {
+    document.getElementById("modal2Login").style.display = "flex";
+  }
+
+  function cerrarModal2() {
+    document.getElementById("modal2Login").style.display = "none";
+  }
+
+  // Cierra al hacer clic fuera del modal
+  
+  window.onclick = function(e) {
+    const modal2 = document.getElementById("modal2Login");
+    if (e.target === modal2) {
+      cerrarModal2();
+    }
+  }
+
+
+
+  const menuToggle = document.getElementById('menu-toggle');
+  const menu = document.querySelector('.menu');
+
+  menuToggle.addEventListener('click', () => {
+      menu.classList.toggle('active');
+  });
+
+  
